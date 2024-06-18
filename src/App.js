1010b892054a12606766be284c2e121de4da381a
TundeAkintola <akintolababatunde557@gmail.com>
2024-06-18 09:11:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Accordion from "./components/Accordion";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const items = [
+    {
+      id: "asdf",
+      label: "Can react be used for web development?",
+      content:
+        "Yes, React is a component based architecture fror front-end web development. Yes, React is a component based architecture fror front-end web development",
+    },
+    {
+      id: "sfegf",
+      label: "Can react handle page routing?",
+      content:
+        "React handles page routing seemlessly with the use of page router library. React handles page routing seemlessly with the use of page router library",
+    },
+    {
+      id: "dfgg",
+      label: "Can I create a Native app with react?",
+      content:
+        "with react and react native library you can create apps that works for both andriod and ios. with react and react native library you can create apps that works for both andriod and ios.",
+    },
+  ];
+  return <Accordion items={items} />;
 }
 
 export default App;
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,49 @@
+import {
+  GoBellFill,
+  GoBook,
+  GoCheckCircleFill,
+  GoCreditCard,
+  GoHeartFill,
+} from "react-icons/go";
+import Button from "../components/Button";
+
+export default function ButtonPage() {
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  return (
+    <div>
+      <div>
+        <Button className="mb-5" success rounded outline onClick={handleClick}>
+          <GoCreditCard />
+          Buy Now!
+        </Button>
+      </div>
+
+      <div>
+        <Button danger outline onMouseEnter={handleClick}>
+          <GoBellFill />
+          Notification
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoHeartFill onMouseLeave={handleClick} />
+          Wishlist
+        </Button>
+      </div>
+      <div>
+        <Button outline secondary>
+          <GoBook />
+          Read
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded outline>
+          <GoCheckCircleFill />
+          Check out!
+        </Button>
+      </div>
+    </div>
+  );
+}
